9354ece179c64070f4eb5e4f4f7e9e230e7a61dc
imdaocuong <82821673+imdaocuong@users.noreply.github.com>
2021-07-27 23:15:41 +0700

    update

--- assets/js/login_form.js
+++ assets/js/login_form.js
@@ -1,12 +1,4 @@
-const btn_OpenLogin = document.querySelector("#show-login");
-const login__inner  = document.querySelector(".hero");
-const btn_CloseLogin = document.querySelector(".close-btn");
-btn_OpenLogin.addEventListener("click",function(){
-    login__inner.classList.add("active");
-})
-btn_CloseLogin.addEventListener("click",function(){
-    login__inner.classList.remove("active");
-})
+
 const btn_OpenEnroll = document.querySelector("#show-enroll");
 const enroll__inner  = document.querySelector(".enroll");
 const btn_CloseEnroll = document.querySelector(".close-enroll");
